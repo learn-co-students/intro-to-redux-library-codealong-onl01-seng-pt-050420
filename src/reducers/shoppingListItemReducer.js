0c9a3d506cdadca364b6ab46b6ae8808175b2928
James Rogers <jarogers095@gmail.com>
2021-02-23 22:34:32 -0800

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,16 +1,15 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+export default function shoppingListItemReducer( state = { items: [] }, action) {
+	console.log(action);
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log("Current state.items length %s", state.items.length);
+			console.log("Updating state.items length to %s", state.items.length + 1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log("Initial state.items leght: %s", state.items.length);
 			return state;
 	}
 }
